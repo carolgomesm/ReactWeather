a25273f5bd715e48f46d7c215381029ecbf820b9
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-11 14:56:54 -0400

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,20 @@
+//Greeter Message component
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var objOne = {
+  name: "Carol",
+  location: "Toronto"
+};
+
+var objTwo = {
+  age: 25,
+  ...objOne
+};
+
+console.log(objTwo);
+
+ReactDOM.render(
+    <h1>Boilerplate app!</h1>,
+    document.getElementById('app')
+);
