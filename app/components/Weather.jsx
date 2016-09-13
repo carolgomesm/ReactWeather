ee508344226431eecef2eeaf6f168b2c580c5e87
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-12 20:57:03 -0400

    Add API call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,18 +1,23 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
   getInitialState: function () {
     return {
-        location: 'Miami',
-        temp: 88
+        
     }
   },
   handleSearch: function (location) {
-    this.setState({
-      location: location,
-      temp: 23
+    var that = this;
+    openWeatherMap.getTemp(location).then(function(temp){
+      that.setState({
+        location: location,
+        temp: temp
+      })
+    }, function(errorMessage){
+      alert(errorMessage);
     });
   },
   render: function () {
