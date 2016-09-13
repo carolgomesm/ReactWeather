0f44a9b5ab6dc7f16d922471b02f90c7412879d4
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-12 21:07:23 -0400

    Add loading message

--- public/bundle.js
+++ public/bundle.js
@@ -24939,24 +24939,44 @@
 	  displayName: 'Weather',
 
 	  getInitialState: function getInitialState() {
-	    return {};
+	    return {
+	      isLoading: false
+	    };
 	  },
 	  handleSearch: function handleSearch(location) {
 	    var that = this;
+
+	    this.setState({ isLoading: true });
+
 	    openWeatherMap.getTemp(location).then(function (temp) {
 	      that.setState({
 	        location: location,
-	        temp: temp
+	        temp: temp,
+	        isLoading: false
 	      });
 	    }, function (errorMessage) {
 	      alert(errorMessage);
+	      that.setState({ isLoading: false });
 	    });
 	  },
 	  render: function render() {
 	    var _state = this.state;
+	    var isLoading = _state.isLoading;
 	    var temp = _state.temp;
 	    var location = _state.location;
 
+
+	    function renderMessage() {
+	      if (isLoading) {
+	        return React.createElement(
+	          'h3',
+	          null,
+	          'Fetching Weather...'
+	        );
+	      } else if (temp && location) {
+	        return React.createElement(WeatherMessage, { temp: temp, location: location });
+	      }
+	    }
 
 	    return React.createElement(
 	      'div',
@@ -24967,7 +24987,7 @@
 	        'Weather Component'
 	      ),
 	      React.createElement(WeatherForm, { onSearch: this.handleSearch }),
-	      React.createElement(WeatherMessage, { temp: temp, location: location })
+	      renderMessage()
 	    );
 	  }
 	});
