f418639c3002854836bbbbfd71cc58ef0a75e593
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-13 20:45:47 -0400

    Refactor stateless components

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,18 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function (){
-    return (
-      <h3>About Component</h3>
-    );
-  }
-});
+// var About = React.createClass({
+//   render: function (){
+//     return (
+//       <h3>About Component</h3>
+//     );
+//   }
+// });
 
+var About = (props) => {
+  return (
+    <div>
+      <h3>This is a weather app built using React</h3>
+    </div>
+  );
+};
 module.exports = About;
