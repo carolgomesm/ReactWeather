8bd25aac2895a988581655bf0189b63bd4c66225
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-18 15:50:49 -0400

    Add nev search support

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -8,10 +8,10 @@ var Example = (props) => {
       <p> Here are a few examples locations to try out:</p>
       <ol>
         <li>
-          <Link className="link" to='/?locaton=Toronto'> Toronto, CA</Link>
+          <Link className="link" to='/?location=Toronto'> Toronto, CA</Link>
         </li>
         <li>
-          <Link className="link" to='/?locaton=Rio'> Rio, Brazil</Link>
+          <Link className="link" to='/?location=Rio'> Rio, Brazil</Link>
         </li>
       </ol>
     </div>
